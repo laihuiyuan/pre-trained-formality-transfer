669f420e185bd4fd7c1d0e04bd51e3715b8de7b4
laihuiyuan <huiyuanlai.l@gmail.com>
2021-06-03 16:32:33 +0200

    update

--- pg.sh
+++ pg.sh
@@ -13,15 +13,15 @@ python infer.py -style 1 -dataset $1 -order $2.0
 rm checkpoints/bart_$1_$2.0_1.chkpt
 
 echo "----------------Style----------------"
-python classifier/test.py -dataset $1 -order $2.0 -model textcnn
+python classifier/test.py -dataset $1 -order $2.0
 
 echo "----------------BLEU----------------"
-python utils/tokenizer.py ../data/outputs/bart_$1_$2.0.0 ../data/$1/outputs/bart_$1_$2.0.0 False
-python utils/tokenizer.py ../data/outputs/bart_$1_$2.0.1 ../data/$1/outputs/bart_$1_$2.0.1 Flase
-perl utils/multi-bleu.perl ../data/$1/original_ref/$5.ref < ../data/$1/outputs/bart_$1_$2.0.0
-perl utils/multi-bleu.perl ../data/$1/original_ref/$6.ref < ../data/$1/outputs/bart_$1_$2.0.1
+python utils/tokenizer.py data/outputs/bart_$1_$2.0.0 data/$1/outputs/bart_$1_$2.0.0 False
+python utils/tokenizer.py data/outputs/bart_$1_$2.0.1 data/$1/outputs/bart_$1_$2.0.1 Flase
+perl utils/multi-bleu.perl data/$1/original_ref/$5.ref < data/$1/outputs/bart_$1_$2.0.0
+perl utils/multi-bleu.perl data/$1/original_ref/$6.ref < data/$1/outputs/bart_$1_$2.0.1
 
 echo "----------------BLEURT----------------"
-python cal_bleurt.py ../data/outputs/bart_$1_$2.0.0 ../data/outputs/bart_$1_$2.0.1 \
-                         ../data/$1/test/$5.ref ../data/$1/test/$6.ref
+python util/cal_bleurt.py data/outputs/bart_$1_$2.0.0 data/outputs/bart_$1_$2.0.1 \
+                         data/$1/test/$5.ref data/$1/test/$6.ref
 
